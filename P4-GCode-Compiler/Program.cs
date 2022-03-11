55693c2b758359a7bc0755bb52f572afcc1ed0d6
Casper-NS <caspernyvang@gmail.com>
2022-03-11 12:59:48 +0100

    Added a solution to our github

---
+++ P4-GCode-Compiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P4_GCode_Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
