51a4cb70a9b08535061b69c95578f2127bd6a46f
henneboy <72788863+henneboy@users.noreply.github.com>
2022-03-18 09:55:46 +0100

    Added a prettyprinter
    It can also handle EOL, and will write EOL instead of making a linebreak.

---
+++ PrettyPrintATestFile/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrettyPrintATestFile
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Insert the name of the file from the CorrectFiles folder you wish to pretty-print
+            PrettyPrintCorrectFile.Print();
+        }
+    }
+}
