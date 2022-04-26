c0913c7c1a9dea0b1f2cb764ce6726828ae4b60b
thom776g <thomas@nordfiber.dk>
2022-04-26 10:40:41 +0200

    Updated and functioning visitor
    Its been updated so its internal.
    also fixed the visitor, so that it works with the right nodes

--- GOAT-Compiler/SymbolTableBuilder.cs
+++ GOAT-Compiler/SymbolTableBuilder.cs
@@ -1,22 +1,25 @@
 ﻿using GOATCode.analysis;
 using GOATCode.node;
 using System;
+using System.Collections.Generic;
 
 namespace GOAT_Compiler
 {
     internal class SymbolTableBuilder : DepthFirstAdapter
     {
 
-        private ISymbolTable _table;
+        internal ISymbolTable _symboltable;
 
-        SymbolTableBuilder(ISymbolTable IST)
+        internal Dictionary<Node, string> _typeTable = new Dictionary<Node, string>();
+
+        internal SymbolTableBuilder(ISymbolTable IST)
         {
-            _table = IST;
+            _symboltable = IST;
         }
 
-        private Types _processNode(Node node)
+        private Types _processTypeOfNode(Node node)
         {
-            switch (node.ToString())
+            switch (_typeTable[node])
             {
                 case "int":
                     return Types.Integer;
@@ -35,52 +38,76 @@ namespace GOAT_Compiler
 
         public override void OutAVarDecl(AVarDecl node)
         {
-            _table.AddSymbol(node.GetId().ToString(), _processNode(node));
+            _symboltable.AddSymbol(node.GetId().ToString(), _processTypeOfNode(node));
+            Console.WriteLine(_symboltable.GetSymbol(node.GetId().ToString()).name);
         }
 
         public override void OutAParamDecl(AParamDecl node)
         {
-            _table.AddSymbol(node.GetId().ToString(), _processNode(node));
+            _symboltable.AddSymbol(node.GetId().ToString(), _processTypeOfNode(node.GetTypes()));
+            Console.WriteLine(_symboltable.GetSymbol(node.GetId().ToString()).name);
         }
 
         public override void OutAFuncDecl(AFuncDecl node)
         {
-            _table.AddSymbol(node.GetId().ToString(), _processNode(node)); ;
+            _symboltable.AddSymbol(node.GetId().ToString(), _processTypeOfNode(node.GetTypes()));
+            Console.WriteLine(_symboltable.GetSymbol(node.GetId().ToString()).name);
         }
 
         public override void OutAProcDecl(AProcDecl node)
         {
-            _table.AddSymbol(node.GetId().ToString(), Types.Void);
+            _symboltable.AddSymbol(node.GetId().ToString(), Types.Void);
+            Console.WriteLine(_symboltable.GetSymbol(node.GetId().ToString()).name);
         }
 
         public override void InAStmtlistBlock(AStmtlistBlock node)
         {
-            _table.OpenScope();
+            _symboltable.OpenScope();
         }
 
         public override void OutAStmtlistBlock(AStmtlistBlock node)
         {
-            _table.CloseScope();
+            _symboltable.CloseScope();
         }
 
         public override void InABuildBlock(ABuildBlock node)
         {
-            _table.OpenScope();
+            _symboltable.OpenScope();
         }
 
         public override void OutABuildBlock(ABuildBlock node)
         {
-            _table.CloseScope();
+            _symboltable.CloseScope();
         }
 
         public override void InAWalkBlock(AWalkBlock node)
         {
-            _table.OpenScope();
+            _symboltable.OpenScope();
         }
 
         public override void OutAWalkBlock(AWalkBlock node)
         {
-            _table.CloseScope();
+            _symboltable.CloseScope();
+        }
+
+        public override void OutAIntTypes(AIntTypes node) 
+        {
+            _typeTable.Add(node.Parent(), "int");
+        }
+
+        public override void OutAFloatTypes(AFloatTypes node)
+        {
+            _typeTable.Add(node.Parent(), "float");
+        }
+
+        public override void OutABoolTypes(ABoolTypes node) 
+        {
+            _typeTable.Add(node.Parent(), "bool");
+        }
+
+        public override void OutAVectorTypes(AVectorTypes node)
+        {
+            _typeTable.Add(node.Parent(), "vector");
         }
     }
 }
