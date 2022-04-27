951f8321b2792f707691e3270d4fad71efa0700e
S3JER <72788875+S3JER@users.noreply.github.com>
2022-04-27 14:13:27 +0200

    Added a new Class TypeChecker

---
+++ GOAT-Compiler/TypeChecker.cs
@@ -0,0 +1,31 @@
+﻿using GOATCode.node;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOAT_Compiler
+{
+    internal class TypeChecker : SymbolTableVisitor
+    {
+        private Dictionary<Node, Types> _typeDictionary = new Dictionary<Node, Types>();
+        public TypeChecker(ISymbolTable symbolTable) : base(symbolTable)
+        {
+        }
+
+        public override void OutAPlusExp(APlusExp node)
+        {
+            Types left = _typeDictionary[node.GetL()];
+            Types right = _typeDictionary[node.GetR()];
+            if (left != right)
+            {
+                throw new Exception("Type mismatch");
+            }
+        }
+        public override void OutANumberExp(ANumberExp node)
+        {
+            _typeDictionary.Add(node, _symbolTable.GetSymbol(node.GetNumber().Text).type);
+        }
+    }
+}
