a4bcff1bb426c2ab54adcd5cbc3e6b006090279d
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-04-28 09:46:03 +0200

    Basic type mismatch exception

---
+++ GOAT-Compiler/TypeMismatchException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOAT_Compiler
+{
+    public class TypeMismatchException : Exception
+    {
+        public TypeMismatchException() { }
+        public TypeMismatchException(string message) : base(message) { }
+        public TypeMismatchException(string message, Exception inner) : base(message, inner) { }
+        protected TypeMismatchException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
