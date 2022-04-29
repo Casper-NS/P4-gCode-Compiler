70f0c2fa344696ce93a5db9054fce1ecbbe38d36
thom776g <thomas@nordfiber.dk>
2022-04-29 11:50:46 +0200

    Moved exceptions to their own file

---
+++ GOAT-Compiler/PopException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GOAT_Compiler
+{
+    internal class PopException : Exception
+    {
+        internal PopException(Extrude e)
+        {
+            Console.WriteLine("Cannot pop " + e);
+        }
+    }
+
+}
---
+++ GOAT-Compiler/PushException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GOAT_Compiler
+{
+    internal class PushException : Exception
+    {
+        internal PushException(Extrude e)
+        {
+            Console.WriteLine("Cannot push " + e);
+        }
+    }
+
+}
