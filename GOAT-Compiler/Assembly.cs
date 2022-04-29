bab2dc861aa755aaaf54de08e78b3bd60ae6a0dc
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-04-29 13:22:32 +0200

    Added the basic system for allowing cleaner tests of file reading

--- GOAT-Compiler/Assembly.cs
+++ GOAT-Compiler/Assembly.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("SymbolTableTest")]
+[assembly: InternalsVisibleTo("VisitorTests")]
---
+++ VisitorTests/Utilities/TestFilesEnumerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorTests
+{
+    internal abstract class BaseFilesEnumerator : IEnumerable<object[]>
+    {
+        public abstract string RelativeFolderPath();
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            foreach (var filePath in GetTestFilesRecursively(FileReadingTestUtilities.ProjectBaseDirectory + RelativeFolderPath()))
+            {
+                yield return filePath;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+
+        private static IEnumerable<object[]> GetTestFilesRecursively(string directoryPath)
+        {
+            // Get all files in root directory
+            foreach (string filePath in Directory.GetFiles(directoryPath))
+            {
+                yield return new object[] { filePath };
+            }
+            // get all subdirectories
+            foreach (string subdirectoryPath in Directory.GetDirectories(directoryPath))
+            {
+                // get everything from current subdirectory
+                foreach (var file in GetTestFilesRecursively(subdirectoryPath))
+                {
+                    yield return file;
+                }
+            }
+
+        }
+    }
+
+}
