8230e2fafc402f1074e9b584a2994eea9f87d51c
S3JER <72788875+S3JER@users.noreply.github.com>
2022-05-02 13:36:44 +0200

    Added two tests
    Added twos test, one that checks if the created file exists and one for checking if empty.

---
+++ VisitorTests/CodeGenerator/CodeGeneratorTest.cs
@@ -0,0 +1,46 @@
+﻿using GOAT_Compiler;
+using GOATCode.node;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorTests.Utilities;
+using Xunit;
+using static GOAT_Compiler.CodeGenerator;
+
+namespace VisitorTests
+{
+    public class CodeGeneratorTest
+    {
+        [SkippableTheory(typeof(TestDependencyException))]
+        [InlineData("RelMove.txt", "RelMove.gcode")]
+        public void CreatedFileChecker(string file, string outPutFileName)
+        {
+            Start s = FileReadingTestUtilities.ParseFile(file);
+            ISymbolTable symbolTable = FileReadingTestUtilities.BuildSymbolTable(s);
+
+            _ = new CodeGenerator(symbolTable, outPutFileName);
+            //Create a assert that checks if the output file exists
+            Assert.True(System.IO.File.Exists(outPutFileName));
+        }
+
+        [SkippableTheory(typeof(TestDependencyException))]
+        [InlineData("RelMove.txt", "RelMove.gcode")]
+        public void CheckIfCreatedFileIsntEmpty(string file, string outPutFileName)
+        {
+            Start s = FileReadingTestUtilities.ParseFile(file);
+            ISymbolTable symbolTable = FileReadingTestUtilities.BuildSymbolTable(s);
+
+            _ = new CodeGenerator(symbolTable, outPutFileName);
+            
+            //Create a assert that checks if the output file is not empty
+            Assert.True(System.IO.File.ReadAllText(outPutFileName).Length > 0);
+        }
+
+    }
+    class CodeGeneratorFilesEnumerator : BaseFilesEnumerator
+    {
+        public override string RelativeFolderPath() => "CodeGenerator/TestFiles";
+    }
+}
