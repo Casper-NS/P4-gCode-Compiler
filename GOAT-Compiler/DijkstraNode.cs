1774a3eb8b9d3c5353cad0f57dfba8f5c9195b65
thom776g <thomas@nordfiber.dk>
2022-05-02 14:24:25 +0200

    Set up buildding a graph for dijkstra search

---
+++ GOAT-Compiler/DijkstraNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace GOAT_Compiler
+{
+    internal class DijkstraNode
+    {
+        private List<DijkstraNode> _functionCalls = new();
+
+        private Extrude _extrudeType;
+
+        internal DijkstraNode(Extrude extrude)
+        {
+            _extrudeType = extrude;
+        }
+
+        internal void AddFunctionCall(DijkstraNode dn)
+        {
+            _functionCalls.Add(dn);
+        }
+
+        internal void SetExtrudeType(Extrude e)
+        {
+            _extrudeType = e;
+        }
+
+        internal Extrude GetExtrudeType()
+        {
+            return _extrudeType;
+        }
+
+        internal void SearchChild() 
+        {
+            
+        }
+    }
+
+}
