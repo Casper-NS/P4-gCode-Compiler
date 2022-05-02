27e6e84fee397ea016d60f39e9bad786786f4dad
Casper-NS <caspernyvang@gmail.com>
2022-05-02 13:55:48 +0200

    Added a dictionary and the symbol table

--- GOAT-Compiler/CodeGenerator.cs
+++ GOAT-Compiler/CodeGenerator.cs
@@ -1,14 +1,19 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using GOATCode.node;
 
 namespace GOAT_Compiler
 {
     internal class CodeGenerator : SymbolTableVisitor
     {
         FileStream gcodeFile;
+        private ISymbolTable _symbolTable;
+        private Dictionary<Symbol, (object, Types)> RunTimeTable = new Dictionary<Symbol, (object, Types)>();
 
         public CodeGenerator(ISymbolTable symbolTable, string outputName) : base(symbolTable)
         {
+            _symbolTable = symbolTable;
             if (outputName.Length == 0)
             {
                 gcodeFile = File.Create("GOAT.gcode");
