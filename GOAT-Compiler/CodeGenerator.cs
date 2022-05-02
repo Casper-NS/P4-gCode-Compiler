5e0df53b82f91a92e8b989f5e1942e395ed6ff0a
Casper-NS <caspernyvang@gmail.com>
2022-05-02 12:56:16 +0200

    Initial commit for the creation of the class

---
+++ GOAT-Compiler/CodeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace GOAT_Compiler
+{
+    internal class CodeGenerator : SymbolTableVisitor
+    {
+        FileStream gcodeFile;
+
+        public CodeGenerator(ISymbolTable symbolTable, string outputName) : base(symbolTable)
+        {
+            if (outputName.Length == 0)
+            {
+                gcodeFile = File.Create("GOAT.gcode");
+            }
+            else
+            {
+                gcodeFile = File.Create(outputName + ".gcode");
+            }
+        }
+    }
+}
