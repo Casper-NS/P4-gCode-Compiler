871feae54270a134229d2fa247fd159463c6c6ae
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-05-03 13:51:19 +0200

    More tests

--- VisitorTests/ExtruderChecker/ExtruderCheckerTest.cs
+++ VisitorTests/ExtruderChecker/ExtruderCheckerTest.cs
@@ -49,8 +49,7 @@ namespace ExtruderCheckerTest
             Start s = FileReadingTestUtilities.ParseFile(filePath);
             ISymbolTable symbolTable = FileReadingTestUtilities.BuildSymbolTable(s);
             ExtruderChecker checker = new ExtruderChecker(symbolTable);
-            s.Apply(checker);
-            //Assert.Throws<CallBuildInWalkException>(() => s.Apply(checker));
+            Assert.Throws<CallBuildInWalkException>(() => s.Apply(checker));
         }
         class ExtruderCheckerThrowsExtruderExceptionFilesEnumerator : BaseFilesEnumerator
         {
