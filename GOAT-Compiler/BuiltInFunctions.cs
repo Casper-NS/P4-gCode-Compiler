a07b91c36e172051a33c8f512ae611f7864253c2
S3JER <72788875+S3JER@users.noreply.github.com>
2022-05-04 14:43:47 +0200

    Create BuiltInFunctions.cs

---
+++ GOAT-Compiler/BuiltInFunctions.cs
@@ -0,0 +1,20 @@
+﻿using GOATCode.node;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOAT_Compiler
+{
+    static class BuiltInFunctions
+    {
+        public static IReadOnlyDictionary<string, List<Types>> FunctionsList => _functionsList;
+        private static Dictionary<string, List<Types>> _functionsList = new Dictionary<string, List<Types>>() 
+        {
+            { "RelMove", new List<Types>() { Types.Void, Types.Vector } },
+            { "AbsMove", new List<Types>() { Types.Void, Types.Vector } },
+        };
+        
+    }
+}
