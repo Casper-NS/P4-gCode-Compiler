23527b1f66348afb9d19cd01a2ca6fabbbdf3f55
S3JER <72788875+S3JER@users.noreply.github.com>
2022-05-05 09:46:33 +0200

    Added all the built in functions to the dictionary

--- GOAT-Compiler/BuiltInFunctions.cs
+++ GOAT-Compiler/BuiltInFunctions.cs
@@ -17,11 +17,26 @@ namespace GOAT_Compiler
         /// </summary>
         internal static IReadOnlyDictionary<string, Symbol> FunctionsList => _functionsList;
         
-        private static Dictionary<string, Symbol> _functionsList = new Dictionary<string, Symbol>() 
+        private static Dictionary<string, Symbol> _functionsList = new() 
         {
             { "RelMove", new Symbol("RelMove", Types.Void, Types.Vector) },
             { "AbsMove", new Symbol("AbsMove", Types.Void, Types.Vector) },
+            { "RelRec", new Symbol("RelRec", Types.Void, Types.FloatingPoint) },
+            { "AbsRec", new Symbol("AbsRec", Types.Void, Types.FloatingPoint) },
+            { "Position", new Symbol("Position", Types.Vector) },
+            { "Steps", new Symbol("Steps", Types.Void, Types.FloatingPoint) },
+            { "Lift", new Symbol("Lift", Types.Void, Types.FloatingPoint) },
+            { "Right", new Symbol("Right", Types.Void, Types.FloatingPoint) },
+            { "Left", new Symbol("Left", Types.Void, Types.FloatingPoint) },
+            { "Direction", new Symbol("Direction", Types.FloatingPoint) },
+            { "TurnTo", new Symbol("TurnTo", Types.Void, Types.FloatingPoint) },
+            { "SetBedTemp", new Symbol("SetBedTemp", Types.Void, Types.FloatingPoint) },
+            { "SetExtruderTemp", new Symbol("SetExtruderTemp", Types.Void, Types.FloatingPoint) },
+            { "WaitForBedTemp", new Symbol("WaitForBedTemp", Types.Void) },
+            { "WaitForExtruderTemp", new Symbol("WaitForExtruderTemp", Types.Void) },
+            { "WaitForCurrentMove", new Symbol("WaitForCurrentMove", Types.Void) },
+            { "WaitForMillis", new Symbol("WaitForMillis", Types.Void, Types.FloatingPoint)},
+            { "SetFanPower", new Symbol("SetFanPower", Types.Void, Types.FloatingPoint)}
         };
-        
     }
 }
