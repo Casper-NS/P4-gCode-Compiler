a6d02945e8224a351be687eb945a93402bcc3ad7
S3JER <72788875+S3JER@users.noreply.github.com>
2022-05-05 15:23:20 +0200

    Update CNCMachine.cs

--- GOAT-Compiler/Code Generation/CNCMachine.cs
+++ GOAT-Compiler/Code Generation/CNCMachine.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GOAT_Compiler.Code_Generation
 {
@@ -178,6 +174,150 @@ namespace GOAT_Compiler.Code_Generation
             {
                 gLine = "G1 X" + ReturnPosition().X + " Y" + ReturnPosition().Y + " Z" + ReturnPosition().Z;
             }
+        }
+        public void RelArc(Vector v, float r)
+        {
+            string gLine = "";
+            Vector v1 = ReturnPosition();
+            if (VectorDistance(v1, v) > r*2)
+            {
+                throw new Exception("RelArc radius is too small.");
+            }
+            UpdatePosition(new Vector(v1.X + v.X, v1.Y + v.Y, v1.Z + v.Z));
+            Vector v2 = ReturnPosition();
+            if (GetMode())
+            {
+                if(r < 0) { 
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G3 X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + " E" + ReturnExtruderRate() + " R" + -r;
+                }
+                else
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G2 X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + " E" + ReturnExtruderRate() + " R" + r;
+                }
+            }
+            else
+            {
+                if (r < 0)
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G3 X" + "X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + ReturnPosition().Z + " R" + -r;
+                }
+                else
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G2 X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + ReturnPosition().Z + " R" + r;
+                }
+            }
+        }
+        private float VectorDistance(Vector v1, Vector v2) 
+        {
+            return (float)Math.Sqrt(Math.Pow(v1.X - v2.X, 2) + Math.Pow(v1.Y - v2.Y, 2) + Math.Pow(v1.Z - v2.Z, 2));
+        }
+
+        public void AbsArc(Vector v, float r)
+        {
+            string gLine = "";
+            Vector v1 = ReturnPosition();
+            if (VectorDistance(v1, v) > r * 2)
+            {
+                throw new Exception("AbsArc radius is too small.");
+            }
+            UpdatePosition(v);
+            Vector v2 = ReturnPosition();
+            if (GetMode())
+            {
+                if (r < 0)
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G3 X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + " E" + ReturnExtruderRate() + " R" + -r;
+                }
+                else
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G2 X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + " E" + ReturnExtruderRate() + " R" + r;
+                }
+            }
+            else
+            {
+                if (r < 0)
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G3 X" + "X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + ReturnPosition().Z + " R" + -r;
+                }
+                else
+                {
+                    AddToExtrusion(ReturnExtruderRate());
+                    gLine = "G2 X" + v2.X + " Y" + v2.Y + " Z" + v2.Z + ReturnPosition().Z + " R" + r;
+                }
+            }
+        }
+        public void SetExtruderTemp(float temp)
+        {
+            string gLine = "";
+            UpdateExtruderTemp(temp);
+            gLine = "M104 S" + ReturnExtruderTemp().ToString();
+        }
+        public void SetFanPower(float power)
+        {
+            string gLine = "";
+            UpdateFanPower(power);
+            gLine = "M106 S" + ReturnFanPower().ToString();
+        }
+        public void SetExtruderRate(float rate)
+        {
+            string gLine = "";
+            UpdateExtruderRate(rate);
+            gLine = "M220 S" + ReturnExtruderRate().ToString();
+        }
+        public void SetBedTemp(float temp)
+        {
+            string gLine = "";
+            UpdateBedTemp(temp);
+            gLine = "M140 S" + ReturnBedTemp().ToString();
+        }
+        public Vector Position(){
+            return ReturnPosition();
+        }
+        public void Steps(float step)//Needs more
+        {
+            //string gLine = "";
+            //UpdatePosition(new Vector(ReturnPosition().X + step, ReturnPosition().Y + step, ReturnPosition().Z + step));
+        }
+        public void Lift(float step)
+        {
+            
+        }
+        public void Right(float deg)
+        {
+            
+        }
+        public void Left(float deg)
+        {
+            
+        }
+        public float Direction()
+        {
+            return ReturnRotation();
+        }
+        public void TurnTo(float deg)
+        {
+            
+        }
+        public void WaitForBedTemp()
+        {
+        }
+        public void WaitForExtruderTemp()
+        {
+        }
+        public void WaitForCurrentMove()
+        {
+            
+        }
+        public void WaitForMillis()
+        {
+            
         }
         #endregion
     }
