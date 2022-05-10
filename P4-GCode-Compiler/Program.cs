c5cf6d8983bab3f3353a6acd70782c955f4e22f1
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-05-10 15:00:10 +0200

    Can now run the compiler from the command line

--- P4-GCode-Compiler/Program.cs
+++ P4-GCode-Compiler/Program.cs
@@ -9,30 +9,50 @@ namespace P4_GCode_Compiler
 {
     class Program
     {
+
+        static void ReadArgs(string[] args, out string fileIn, out string fileOut)
+        {
+            if (args.Length != 2)
+            {
+                throw new ArgumentException("Wrong arguments. Usage: GOAT inputFile outputFile");
+            }
+            fileIn = args[0];
+            fileOut = args[1];
+        }
+
         static void Main(string[] args)
         {
-            ISymbolTable symTab = new RecSymbolTable();
-            StreamReader reader = new StreamReader("../../../../visitorTests/Test.txt");
+            ReadArgs(args, out string fileIn, out string fileOut);
+
+            ReadAndGenerateAST(fileIn, out Start AST, out ISymbolTable symTable, out TypeChecker typeChecker);
+
+            GenerateCode(fileOut, AST, symTable, typeChecker);
+        }
+
+        static void ReadAndGenerateAST(string file, out Start AST, out ISymbolTable symTable, out TypeChecker typeChecker)
+        {
+            symTable = new RecSymbolTable();
+            StreamReader reader = new StreamReader(file);
             Lexer l = new Lexer(reader);
             Parser p = new Parser(l);
-            Start S = p.Parse();
-            SymbolTableBuilder builder = new SymbolTableBuilder(symTab);
-            S.Apply(builder);
-            TypeChecker Tchecker = new TypeChecker(symTab);
-            S.Apply(Tchecker);
-            ScopeChecker scopeChecker = new ScopeChecker(symTab);
-            S.Apply(scopeChecker);
-            CodeGenerator codeGenerator;
-            using (File.Create("test.gcode")) { }
+            AST = p.Parse();
+            SymbolTableBuilder builder = new SymbolTableBuilder(symTable);
+            AST.Apply(builder);
+            typeChecker = new TypeChecker(symTable);
+            AST.Apply(typeChecker);
+            ScopeChecker scopeChecker = new ScopeChecker(symTable);
+            AST.Apply(scopeChecker);
+        }
 
-            using (Stream stream = new FileStream("test.gcode", FileMode.Open))
+        static void GenerateCode(string file, Start AST, ISymbolTable symTable, TypeChecker typeChecker)
+        {
+            using (Stream stream = new FileStream(file, FileMode.OpenOrCreate))
             {
                 using (StreamWriter writer = new StreamWriter(stream))
                 {
-                    codeGenerator = new CodeGenerator(symTab, Tchecker.GetTypeDictionary(), writer);
-                    S.Apply(codeGenerator);
+                    CodeGenerator codeGenerator = new CodeGenerator(symTable, typeChecker.GetTypeDictionary(), writer);
+                    AST.Apply(codeGenerator);
                 }
-
             }
         }
     }
